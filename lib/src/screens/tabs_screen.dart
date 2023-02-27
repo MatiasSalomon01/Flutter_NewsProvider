433459d07d58fc8664e039c81643181defeed007
MatiasSalomon01 <matisalomond@gmail.com>
2023-02-27 14:37:58 -0300

    Implementado el BottomNavigationBar

--- lib/src/screens/tabs_screen.dart
+++ lib/src/screens/tabs_screen.dart
@@ -5,20 +5,56 @@ class TabsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: [
-          Container(
-            width: double.infinity,
-            color: Colors.red,
-          ),
-          Container(
-            width: double.infinity,
-            color: Colors.blue,
-          )
-        ],
-      )
+    return const Scaffold(
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
    );
+  }
+}
+
+class _Navegacion extends StatelessWidget {
+  const _Navegacion({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Para Ti',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.public),
+          label: 'Encabezados'
+        )
+      ]
+    );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  const _Paginas({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      //physics: BouncingScrollPhysics(),
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        Container(
+          width: double.infinity,
+          color: Colors.red,
+        ),
+        Container(
+          width: double.infinity,
+          color: Colors.blue,
+        )
+      ],
+    );
   }
 }
